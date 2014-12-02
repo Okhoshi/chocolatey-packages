77fe04d6d61f0cb16d34e7372dce1ed166fb6964
Quentin Devos <uw2k7_saotome@live.be>
2014-12-02 21:51:05 +0100

    Update GeForce-Experience and Shairport4W

--- shairport4w/tools/chocolateyInstall.ps1
+++ shairport4w/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'shairport4w' # arbitrary name for the package, used in messages
-$url = 'http://sourceforge.net/projects/shairport4w/files/latest/download' # download url
+$url = 'http://sourceforge.net/projects/shairport4w/files/SP4W_Setup.exe/download' # download url
 $installerType = 'EXE' #only one of these: exe, msi, msu
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
