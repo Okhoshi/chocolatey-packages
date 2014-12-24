bc4aa29a6c3f894064e4d03585f6c27711f9faa1
Quentin Devos <uw2k7_saotome@live.be>
2014-12-24 10:12:09 +0100

    Deprecate WP Project Screen Fix Shairport install error add z1c0 repo as submodule

--- shairport4w/tools/chocolateyInstall.ps1
+++ shairport4w/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
 $url = 'http://sourceforge.net/projects/shairport4w/files/SP4W_Setup.exe/download' # download url
 $installerType = 'EXE' #only one of these: exe, msi, msu
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$validExitCodes = @(0, 2) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
