5e1b73358d745b879a3002685694852de0e34663
Quentin Devos <Uw2k7_saotome@live.be>
2016-01-10 12:19:59 +0000

    Fixes OpenVPN

--- openvpn/tools/chocolateyInstall.ps1
+++ openvpn/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'EXE' #only one of these: exe, msi, msu
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '/S /SELECT_OPENSSL_UTILITIES=1 /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/S /SELECT_SHORTCUTS=0 /SELECT_SERVICE=0 /SELECT_OPENSSLDLLS=0 /SELECT_LZODLLS=0 /SELECT_PKCS11DLLS=0 /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
