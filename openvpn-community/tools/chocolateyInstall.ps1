78f019f1ba4813291238aed323a54310fceeaeb3
Quentin Devos <Uw2k7_saotome@live.be>
2016-01-11 09:56:21 +0000

    Improvements

--- openvpn-community/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'openvpn-community' # arbitrary name for the package, used in messages
-$installerType = 'EXE' #only one of these: exe, msi, msu
-$url = '{{DownloadUrl}}' # download url
-$url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '/S /SELECT_OPENSSL_UTILITIES=1 /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- wp-project-screen/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'wp-project-screen'
-$installerType = 'EXE'
-$url = 'http://www.microsoft.com/en-us/download/confirmation.aspx?id=42536'
-$silentArgs = '/qn'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
