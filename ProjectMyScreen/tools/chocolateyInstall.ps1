eda2de8174272cc8182349b6f253bd1118642e39
Quentin Devos <Uw2k7_saotome@live.be>
2016-01-11 10:06:24 +0000

    Import ProjectMyScreen

---
+++ ProjectMyScreen/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'ProjectMyScreen'
+$installerType = 'msi'
+$url = 'https://download.microsoft.com/download/A/2/7/A271EFFF-6C9E-4E9B-9259-0F72FDEDD153/ProjectMyScreenApp.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
