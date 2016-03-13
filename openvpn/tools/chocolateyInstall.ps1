768b289b2b7d7a63325c3227e8220bd792b7ef3b
Quentin D <Okhoshi@users.noreply.github.com>
2016-03-13 09:11:52 +0100

    Re-enable the Service option

--- openvpn/tools/chocolateyInstall.ps1
+++ openvpn/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'EXE' #only one of these: exe, msi, msu
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '/S /SELECT_SERVICE=0 /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/S /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\openvpn.cer'"
