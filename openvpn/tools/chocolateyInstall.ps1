f98f20ee4a558ae70b4e66f34c6eb2adc949b059
Quentin Devos <Uw2k7_saotome@live.be>
2016-06-20 21:42:51 +0000

    Update following the recommendations

--- openvpn/tools/chocolateyInstall.ps1
+++ openvpn/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-﻿$packageName = 'openvpn' # arbitrary name for the package, used in messages
+﻿$packageName = 'openvpn'
 $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'EXE' #only one of these: exe, msi, msu
-$url = '{{DownloadUrl}}' # download url
-$url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '/S /SELECT_SERVICE=0 /SELECT_EASYRSA=1' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '/S /SELECT_EASYRSA=1'
+$validExitCodes = @(0)
 
 Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\openvpn.cer'"
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
